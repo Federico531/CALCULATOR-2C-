f5aebedc25cb0922c23b5b2e127981ba7801cc70
unknown <federicomarchetta@yahoo.com.ar>
2021-03-28 19:40:39 -0300

    2 + 2 = 4 + 4222 FIX

--- combinandoApp.js
+++ combinandoApp.js
@@ -33,7 +33,6 @@ class Value {
 const v = new Value()
 const ui = new UI()
 const h = new History()
-
 class Operation {
     sum(a, b) {
         v.total = a + b
@@ -55,56 +54,59 @@ class Operation {
 
     }
 }
-
-document.getElementById("ON/C").addEventListener('click', () => {
-    //esto se podria meter en una funcion luego
-    v.total = 0;
-    ui.display = 0
-    h.values = [];
-    console.log("prendido")
-})
-document.getElementById("OFF").addEventListener('click', () => {
-    //MAKE TOAST SI NO PRENDE EN 10 SEGUNDOS O HACEN CLICK CON LA CALCU APAGADA
-    v.total = "";
-    h.values = [];
-    console.log("apagado")
-})
+//CONCATENATE DIGIT
+//numA numB tienen que ser parseFloat si o si para hacer test?
+//si no son parseFloat de entrada hay que transformarlos luego
+//y silo asigno a total como String para que se vaya concatenando hasta llegar uny luego lo convierto en float
 
 function asignDigit(digit) {
+    //Esto va a traer un bug pero lo arreglo con un counter
+    if (v.total == 0) {
+        v.total = ""
+    }
     if (!v.operator && isNumber(digit)) {
-        v.numA = parseFloat(digit);
-        v.total = v.numA;
+        v.total += "" + parseFloat(digit);
+        v.numA = parseFloat(v.total)
         ui.display = v.total;
         console.log("assigned numA: " + v.numA)
 
-    } else if (v.numA && isSymbol(digit)) {
+    } else if (v.operator && isNumber(digit)) {
+        v.lastDigit = digit;
+        //COMO HAGO QUE SE AÑADAN DIGITOS EN NUMB HASTA QUE TOQUE OTRA VEZ UN SIMBOLO?
+        if (!v.numB) {
+            v.numB = ""
+        }
+        ui.display = "";
+        v.numB += "" + parseFloat(digit);
+        ui.display += v.numB;
+        v.numB = parseFloat(v.numB)
+        console.log("assigned numB: " + v.numB)
+        whichOperationIs(v.operator)
+        v.numA = v.total
+        console.log("Result: " + v.total)
 
+    } else if (v.numA && isSymbol(digit)) {
         ui.display = v.total
         v.operator = digit
         if (v.lastOperator == v.operator && isSymbol(v.lastDigit)) {
             alert("repeats " + v.lastOperator)
         } else {
+
             v.lastDigit = digit;
             v.lastOperator = v.operator;
             console.log("assigned operator: " + v.operator)
-        }
+            
 
-    } else if (v.numA && !v.numB && isNumber(digit)) {
-        if (isNumber(v.lastDigit)) {
-            console.log("Corresponde ingresar un operador")
-        } else {
-            v.lastDigit = digit
-            v.numB = parseFloat(digit)
-            ui.display = v.numB
-            console.log("assigned numB: " + v.numB)
-            whichOperationIs(v.operator)
-            v.numA = v.total
-            v.operator = "";
-            v.numB = "";
-            console.log("Result: " + v.total)
         }
+    } else if (v.numA && v.numB && isSymbol(digit)) {
+        // if (isNumber(v.lastDigit)) {
+        //     console.log("Corresponde ingresar un operador")
+        // } else {
+        console.log()
+
     }
 }
+
 function isNumber(digit) {
     for (var i = 0; i < 10; i++) {
         var number = digit.includes(i.toString());
@@ -146,7 +148,6 @@ function whichOperationIs(digit) {
         default: alert('Typed a number when should have typed symbol');
     }
 }
-
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('calculator').addEventListener('click', (e) => {
         var digit = e.target.childNodes[0].nodeValue
@@ -161,7 +162,19 @@ document.addEventListener('DOMContentLoaded', function () {
         */
     })
 });
-
+document.getElementById("ON/C").addEventListener('click', () => {
+    //esto se podria meter en una funcion luego
+    v.total = 0;
+    ui.display = 0
+    h.values = [];
+    console.log("prendido")
+})
+document.getElementById("OFF").addEventListener('click', () => {
+    //MAKE TOAST SI NO PRENDE EN 10 SEGUNDOS O HACEN CLICK CON LA CALCU APAGADA
+    v.total = "";
+    h.values = [];
+    console.log("apagado")
+})
 const operatione = new Operation()
 
 module.exports = {
