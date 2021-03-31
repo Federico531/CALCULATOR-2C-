25f4e85f9ec7199a88ebdb206ec6bbfcafd12662
unknown <federicomarchetta@yahoo.com.ar>
2021-03-30 23:35:25 -0300

    funcionando de nuevo

--- combinandoApp.js
+++ combinandoApp.js
@@ -15,7 +15,7 @@ class History {
     constructor() {
         this.anyDigit = [];
         this.total = [];
-        //        this.repeatsEqual = false;
+        // this.repeatsEqual = false;
         this.wasEqualBefore = false;
         this.wasNumberBefore = false;
     }
@@ -44,10 +44,11 @@ const operation = new Operation()
 
 function asignDigit(digit) {
     //separar lo que hay adentro en funciones
-    if (!v.numA) return v.total = "";
-    if (!v.operator && isNumber(digit)) return firstDigitIs(digit)
-    else if (v.operator && isNumber(digit)) return secondDigitIs(digit)
-    else if (isSymbol(digit)) return doOperation(digit)
+    if (!v.numA) { v.total = "" }
+    if (!v.operator && isNumber(digit)) { firstDigitIs(digit) }
+    else if (v.operator && isNumber(digit)) { secondDigitIs(digit) }
+    else if (isSymbol(digit)) { doOperation(digit) }
+
 }
 
 let firstDigitIs = (digit) => {
@@ -72,14 +73,16 @@ let secondDigitIs = (digit) => {
 }
 
 let doOperation = (digit) => {
+
     ui.display = v.total
     v.operator = digit
 
     if (v.lastOperator == v.operator && !digit.includes('=') && !h.wasEqualBefore && !h.wasNumberBefore) {
         alert("repeats " + v.lastOperator)
+
     } else if (digit.includes('=')) {
         h.display = ""
-        console.log("Reassigned operator" + v.lastOperator + "through equal: ")
+        console.log("Reassigned operator" + v.lastOperator + "through equal")
         if (h.wasEqualBefore) {
             console.log("pasa por aca")
             v.numB = parseFloat(v.lastDigit)
@@ -186,4 +189,3 @@ function operation(numA, operator, numB) {
     }
 }
 */
-
